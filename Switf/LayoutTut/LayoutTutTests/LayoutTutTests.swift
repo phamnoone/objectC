f522aaa760cde94363b11fc42932d28eb53dc842
Thanh Pham <thanh@Kaopizs-Mac-mini-2.local>
2015-11-19 14:13:38 +0700

    ignore untracked files

---
+++ Switf/LayoutTut/LayoutTutTests/LayoutTutTests.swift
@@ -0,0 +1,36 @@
+//
+//  LayoutTutTests.swift
+//  LayoutTutTests
+//
+//  Created by Thanh Pham on 11/18/15.
+//  Copyright (c) 2015 Thanh Pham. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LayoutTutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
